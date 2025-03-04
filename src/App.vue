efd931a32373156477464010baeafc5b5c51e345
minorn <1481700559@qq.com>
2025-03-04 22:16:50 +0800

    feat: 添加路径别名、添加列表组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
+<script setup lang="ts">
+import { ConversationProps } from '@/types'
+import ConversationList from '@/components/ConversationList.vue'
+
+const items: ConversationProps[] = [
+  {
+    id: 1,
+    title: 'Conversation 1',
+    providerId: 1,
+    selectedModel: 'Model 1',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
+  {
+    id: 2,
+    title: 'Conversation 2',
+    providerId: 1,
+    selectedModel: 'Model 2',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
+  {
+    id: 3,
+    title: 'Conversation 3',
+    providerId: 1,
+    selectedModel: 'Model 3',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
+]
+</script>
+
 <template>
   <div class="flex items-center justify-between h-screen">
-    <div class="bg-gray-300 text-gray-700 w-[10rem] h-full">sider</div>
-    <div class="bg-green-300 text-blue-700 h-full flex-1">content</div>
+    <div class="w-[300px] bg-gray-200 h-full border-r border-gray-300">
+      <ConversationList :items="items" />
+    </div>
+    <div class="h-full flex-1"></div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
---
+++ src/components/ConversationList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ConversationProps } from '@/types'
+
+defineProps<{ items: ConversationProps[] }>()
+</script>
+
+<template>
+  <div class="conversation-list">
+    <div
+      class="item border-gray-300 border-t cursor-pointer bg-white hover:bg-gray-200 p-2"
+      v-for="item in items"
+      :key="item.id"
+    >
+      <a href="#">
+        <div
+          class="flex justify-between items-center text-sm leading-5 text-gray-500"
+        >
+          <span>{{ item.selectedModel }}</span>
+          <span>{{ item.updatedAt }}</span>
+        </div>
+        <h2
+          class="font-bold leading-6 text-gray-900 truncate"
+          :title="item.title"
+        >
+          {{ item.title }}
+        </h2>
+      </a>
+    </div>
+  </div>
+</template>
