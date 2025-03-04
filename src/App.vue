80e88b7d6b64be373603caa2ad00e82edde1b188
minorn <1481700559@qq.com>
2025-03-04 23:10:51 +0800

    feat: 添加iconify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ConversationProps } from '@/types'
+import { Icon } from '@iconify/vue'
 import ConversationList from '@/components/ConversationList.vue'
 
 const items: ConversationProps[] = [
@@ -27,13 +28,53 @@ const items: ConversationProps[] = [
     createdAt: '2021-10-01',
     updatedAt: '2021-10-01',
   },
+  {
+    id: 4,
+    title: 'Conversation 3',
+    providerId: 1,
+    selectedModel: 'Model 3',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
+  {
+    id: 5,
+    title: 'Conversation 3',
+    providerId: 1,
+    selectedModel: 'Model 3',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
+  {
+    id: 6,
+    title: 'Conversation 3',
+    providerId: 1,
+    selectedModel: 'Model 3',
+    createdAt: '2021-10-01',
+    updatedAt: '2021-10-01',
+  },
 ]
 </script>
 
 <template>
   <div class="flex items-center justify-between h-screen">
     <div class="w-[300px] bg-gray-200 h-full border-r border-gray-300">
-      <ConversationList :items="items" />
+      <div class="h-[90%] overflow-y-auto">
+        <ConversationList :items="items" />
+      </div>
+      <div class="h-[10%] grid grid-cols-2 gap-2 p-2">
+        <button
+          class="shadow-sm inline-flex items-center justify-center bg-green-700 text-white hover:bg-green-700/90 border border-green-700 h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+        >
+          <Icon icon="radix-icons:chat-bubble" class="mr-2" />
+          新建聊天
+        </button>
+        <button
+          class="shadow-sm inline-flex items-center justify-center bg-green-50 text-green-700 hover:bg-green-700 border hover:text-white border-green-700 h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+        >
+          <Icon icon="radix-icons:gear" class="mr-2" />
+          应用设置
+        </button>
+      </div>
     </div>
     <div class="h-full flex-1"></div>
   </div>
