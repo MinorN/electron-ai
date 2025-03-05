67915df316682591b853ee8d3f4ff5e6a9b7e236
minorn <1481700559@qq.com>
2025-03-05 21:50:22 +0800

    style: 修改下拉样式

--- src/components/ProviderSelect.vue
+++ src/components/ProviderSelect.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ProviderProps } from '@/types'
+import { Icon } from '@iconify/vue'
 import {
   SelectContent,
   SelectGroup,
@@ -22,26 +23,38 @@ defineProps<{ items: ProviderProps[] }>()
 
 <template>
   <SelectRoot>
-    <SelectTrigger>
+    <SelectTrigger
+      class="flex w-full items-center justify-between + rounded-md py-1.5 px-3 shadow-sm border outline-none data-[placeholder]:text-gray-400"
+    >
       <SelectValue placeholder="请选择模型" />
+      <Icon icon="radix-icons:chevron-down" class="h-5 w-5" />
     </SelectTrigger>
     <SelectPortal>
-      <SelectContent>
-        <SelectViewport>
-          <div v-for="provide in items" :key="provide.id">
-            <SelectLabel>
-              <img :src="provide.avatar" :alt="provide.name" />
-              {{ provide.name }}
+      <SelectContent class="bg-white rounded-md shadow-md z-[100] border">
+        <SelectViewport class="p-2">
+          <div v-for="provider in items" :key="provider.id">
+            <SelectLabel class="flex items-center px-6 h-7 text-gray-500">
+              <img
+                :src="provider.avatar"
+                :alt="provider.name"
+                class="h-5 w-5 mr-2 rounded"
+              />
+              {{ provider.name }}
             </SelectLabel>
             <SelectGroup>
               <SelectItem
-                v-for="(model, index) in provide.models"
+                v-for="(model, index) in provider.models"
                 :key="index"
                 :value="model"
+                class="outline-none rounded flex items-center h-7 px-6 relative text-green-700 cursor-pointer data-[highlighted]:bg-green-700 data-[highlighted]:text-white"
               >
+                <SelectItemIndicator class="absolute left-2 w-6">
+                  <Icon icon="radix-icons:check" />
+                </SelectItemIndicator>
                 <SelectItemText>{{ model }}</SelectItemText>
               </SelectItem>
             </SelectGroup>
+            <SelectSeparator class="h-[1px] my-2 bg-gray-300" />
           </div>
         </SelectViewport>
       </SelectContent>
