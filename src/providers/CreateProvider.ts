ab432019f14d15118dce84b9ddfa7ea4e757cf6d
minorn <1481700559@qq.com>
2025-03-25 20:01:27 +0800

    feat: 添加deepseek“

--- src/providers/CreateProvider.ts
+++ src/providers/CreateProvider.ts
@@ -1,15 +1,20 @@
-import { BaseProider } from "./BaseProvider"
-import { QianfanProvider } from "./QianfanProvider"
-import { OpenAIProvider } from "./OpenAIProvider"
+import { BaseProider } from './BaseProvider'
+import { QianfanProvider } from './QianfanProvider'
+import { OpenAIProvider } from './OpenAIProvider'
 
 export function CreateProvider(providerName: string): BaseProider {
   switch (providerName) {
-    case "qianfan":
+    case 'qianfan':
       return new QianfanProvider()
-    case "dashscope":
+    case 'dashscope':
       return new OpenAIProvider(
         process.env.ALI_API_KEY as string,
-        "https://dashscope.aliyuncs.com/compatible-mode/v1"
+        'https://dashscope.aliyuncs.com/compatible-mode/v1'
+      )
+    case 'deepseek':
+      return new OpenAIProvider(
+        process.env.DEEP_SEEK_KEY as string,
+        'https://api.deepseek.com'
       )
     default:
       throw new Error(`Unknown provider: ${providerName}`)
